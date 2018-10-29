40a4d9118e25cacc8ce612355906a49a1b4429ec
Mick22121987 <m.vanaelst@wavenet.be>
2018-10-29 11:52:06 +0100

    F8 L1-2 Introduction Observables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Observable, Observer, Subscription } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Observables';
+export class AppComponent implements OnInit, OnDestroy {
+
+  ngOnInit(): void {
+    const obs = Observable.create((observer: Observer<any>) => {
+      const data = 2;
+      observer.next(2);
+      observer.next(3);
+      observer.next(4);
+
+      setTimeout(() => {
+        observer.next(42);
+      }, 2000);
+      observer.error('error');
+      observer.complete();
+    });
+
+    const sub = obs.subscribe({
+      next: x => console.log('[next] : ', x),
+      complete: () => console.log('[complete]'),
+      error: e => console.log('[error] : ', e)
+    });
+
+    sub.unsubscribe();
+
+    const sub2 = obs.subscribe((x) => {
+      console.log(x);
+    }, (e) => {
+      console.log(e);
+    });
+
+    sub2.unsubscribe();
+  }
+
+  ngOnDestroy(): void {
+
+  }
 }
