5145cdaadf804cf35e80aadb9709723781568af0
Mick22121987 <m.vanaelst@wavenet.be>
2018-10-31 15:21:48 +0100

    L5 - Les pipes rxjs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Observable, Observer, Subject, Subscription, BehaviorSubject } from 'rxjs';
+import { map, filter } from 'rxjs/operators';
+interface User {
+  name: string;
+  age: number;
+  email: string;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit, OnDestroy {
+  public email: string;
 
   ngOnInit(): void {
     // Gestion des observables
@@ -41,26 +48,33 @@ export class AppComponent implements OnInit, OnDestroy {
 
     // Gestion des subjects
       console.log('Gestion des subjects');
-      const subj = new BehaviorSubject<number>(0);
+      const subj = new BehaviorSubject<User>({
+        name: 'Jean',
+        age: 28,
+        email: 'jean@gmail.com'
+      });
 
-    subj.next(1);
+    // subj.next(1);
 
-    const s1 = subj.subscribe( (x: number) => {
-      console.log('[S1] : ', x);
+    const s1 = subj.pipe(
+      filter( (user: User) => {
+        return false;
+      }),
+      map( (user: User) => {
+        return user.email;
+      })
+    ).subscribe( (email: string) => {
+      // Utilisation du pipe
+      this.email = email;
+      console.log(email);
     });
-
     // subj.next(2);
-
-    const s2 = subj.subscribe( (x: number) => {
+    const s2 = subj.subscribe( (x: User) => {
       console.log('[S2] : ', x);
     });
-
     // subj.next(3);
-
     s1.unsubscribe();
-
     // subj.next(4);
-
   }
 
   ngOnDestroy(): void {
